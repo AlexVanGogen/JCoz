d87fa241c4c8da8066fc1517c8c11da26ed4713a
alexvangogen <alexvangogen@gmail.com>
2020-08-26 23:20:59 +0300

    [tools] [progress point finder] Add agent class that does nothing

---
+++ tools/progress-point-finder/src/main/kotlin/edu/avgogen/jcoz/tools/ppfinder/ProgressPointFinderAgent.kt
@@ -0,0 +1,12 @@
+package edu.avgogen.jcoz.tools.ppfinder
+
+import java.lang.instrument.Instrumentation
+
+class ProgressPointFinderAgent {
+
+    companion object {
+        @JvmStatic
+        fun premain(agentArgs: String?, instrumentation: Instrumentation) {
+        }
+    }
+}
