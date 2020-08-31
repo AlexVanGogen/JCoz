8858933b49b7b45afba735cd4edb5bf8603662d2
alexvangogen <alexvangogen@gmail.com>
2020-08-31 12:16:35 +0300

    [tools] [progress point finder] Measure number of hits and max interval between hits
    Maybe interval will be replaced with more general statistics, if overhead (and memory consumption) will allow it.

--- tools/progress-point-finder/src/main/java/edu/avgogen/jcoz/tools/ppfinder/HitsRecorder.java
+++ tools/progress-point-finder/src/main/java/edu/avgogen/jcoz/tools/ppfinder/HitsRecorder.java
@@ -2,21 +2,43 @@ package edu.avgogen.jcoz.tools.ppfinder;
 
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class HitsRecorder {
-    private static final ConcurrentHashMap<String, HashMap<Integer, Integer>> hits = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, HashMap<Integer, SourceLineHitsStatistics>> hits = new ConcurrentHashMap<>();
+    private static final AtomicLong timer = new AtomicLong(0);
 
     static {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> hits.forEach((className, lineHits) -> {
             System.out.println("Class " + className);
-            lineHits.forEach((lineNumber, hits) -> System.out.println("\tline " + lineNumber + ": " + hits));
+            lineHits.forEach((lineNumber, hits) -> System.out.println("\tline " + lineNumber + ": " + hits.toString()));
         })));
     }
 
     public static void registerHit(String className, int lineNumber) {
+        long time = timer.incrementAndGet();
         hits.putIfAbsent(className, new HashMap<>());
-        synchronized (hits.get(className)) {
-            hits.get(className).compute(lineNumber, (ignored, b) -> b == null ? 1 : b + 1);
+        HashMap<Integer, SourceLineHitsStatistics> classStatistics = hits.get(className);
+        synchronized (hits) {
+            classStatistics.putIfAbsent(lineNumber, new SourceLineHitsStatistics());
+            classStatistics.get(lineNumber).registerHit(time);
+        }
+    }
+
+    private static class SourceLineHitsStatistics {
+        private int hits;
+        private long lastHitTime;
+        private long maxInterval;
+
+        public void registerHit(long time) {
+            hits++;
+            maxInterval = Math.max(maxInterval, time - lastHitTime);
+            lastHitTime = time;
+        }
+
+        @Override
+        public String toString() {
+            return hits + " hits, " + maxInterval + " max interval";
         }
     }
 }
